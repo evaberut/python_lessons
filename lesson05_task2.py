7dc79363b5b121eaa06746ea81c899508fa8b689
Nora <evaberut@gmail.com>
2025-05-10 10:49:19 +0700

    Добавлены актуальные файлы Selenium для 5-го урока

---
+++ lesson05_task2.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+service = ChromeService(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+service = ChromeService(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+try:
+    driver.get("http://uitestingplayground.com/dynamicid")
+
+    blue_button = driver.find_element(
+        By.XPATH,
+        "//button[text()='Button with Dynamic ID']"
+    )
+
+    blue_button.click()
+
+    time.sleep(1)
+
+finally:
+    driver.quit()
