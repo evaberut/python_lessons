198a895eaa19cdfe827163b8c55a9b46811bb126
evaberut <evaberut@gmail.com>
2025-05-11 15:54:25 +0700

    Update lesson06_task3.py

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -11,14 +11,25 @@ try:
 
     wait = WebDriverWait(driver, 20)
     images = driver.find_elements(By.TAG_NAME, 'img')
-    for image in images:
-        wait.until(
-            lambda driver: driver.execute_script(
-                'return arguments[0].complete '
-                '&& arguments[0].naturalWidth > 0',
-                image
-             )
-        )
+
+    if len(images) >= 3:
+        for image in images:
+            wait.until(
+                lambda driver: driver.execute_script(
+                    'return arguments[0].complete '
+                    '&& arguments[0].naturalWidth > 0',
+                    image
+                ),
+                f"Изображение не загрузилось: {image.get_attribute('src')}"
+            )
+
+        third_image_src = images[2].get_attribute('src')
+        print(f"URL третьего изображения: {third_image_src}")
+    else:
+        print("На странице меньше трех изображений.")
+
+finally:
+    driver.quit()
 
     third_image_src = images[2].get_attribute('src')
 
